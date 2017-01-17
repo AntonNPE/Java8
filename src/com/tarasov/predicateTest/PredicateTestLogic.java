5493751d3c313f9bbdf3d55e7d599fcc37ac42f5
AntonWork <cubsplanet@gmail.com>
2017-01-17 12:24:08 +0200

    added  predicateTest package

---
+++ src/com/tarasov/predicateTest/PredicateTestLogic.java
@@ -0,0 +1,17 @@
+package com.tarasov.predicateTest;
+
+
+import java.util.function.Predicate;
+
+public class PredicateTestLogic {
+
+
+    public static <T> void predicateTestLogic (Predicate <T> predicate, T value ) {
+
+        if (predicate.test(value)){
+            System.out.println("True");
+        }
+        else System.out.println("False");
+
+    }
+}
